4a55d504cac6e4429a38a4b4b03419c604b68dff
TobiramaS <mkoryakovskyyoff@gmail.com>
2022-01-16 15:18:00 +0100

    reblure only 1 funk

--- main.js
+++ main.js
@@ -1,41 +1,56 @@
 window.onload = init;
 
+// function init() {
+//   var image0 = document.getElementById("zero");
+//   image0.onclick = showImageZero;
+//   var image1 = document.getElementById("one");
+//   image1.onclick = showImageOne;
+//   var image2 = document.getElementById("two");
+//   image2.onclick = showImageTwo;
+//   var image3 = document.getElementById("three");
+//   image3.onclick = showImageThree;
+//   var image4 = document.getElementById("four");
+//   image4.onclick = showImageFour;
+//   var image5 = document.getElementById("five");
+//   image5.onclick = showImageFive;
+// }
+
 function init() {
-  var image0 = document.getElementById("zero");
-  image0.onclick = showImageZero;
-  var image1 = document.getElementById("one");
-  image1.onclick = showImageOne;
-  var image2 = document.getElementById("two");
-  image2.onclick = showImageTwo;
-  var image3 = document.getElementById("three");
-  image3.onclick = showImageThree;
-  var image4 = document.getElementById("four");
-  image4.onclick = showImageFour;
-  var image5 = document.getElementById("five");
-  image5.onclick = showImageFive;
+  var images = document.getElementsByTagName("img");
+  for (var i = 0; i < images.length++; i++) {
+    images[i].onclick = showAnswer;
+  }
 }
 
-function showImageZero() {
-  var image0 = document.getElementById("zero");
-  image0.src = "photo/zero.jpg";
+function showAnswer(eventObj) {
+  var image = eventObj.target;
+  var name = image.id;
+  name = "photo/" + name + ".JPG";
+  image.src = name;
 }
-function showImageOne() {
-  var image1 = document.getElementById("one");
-  image1.src = "photo/one.jpg";
-}
-function showImageTwo() {
-  var image2 = document.getElementById("two");
-  image2.src = "photo/two.jpg";
-}
-function showImageThree() {
-  var image3 = document.getElementById("three");
-  image3.src = "photo/three.jpg";
-}
-function showImageFour() {
-  var image4 = document.getElementById("four");
-  image4.src = "photo/four.jpg";
-}
-function showImageFive() {
-  var image5 = document.getElementById("five");
-  image5.src = "photo/five.jpg";
-}
+
+
+// function showImageZero() {
+//   var image0 = document.getElementById("zero");
+//   image0.src = "photo/zero.jpg";
+// }
+// function showImageOne() {
+//   var image1 = document.getElementById("one");
+//   image1.src = "photo/one.jpg";
+// }
+// function showImageTwo() {
+//   var image2 = document.getElementById("two");
+//   image2.src = "photo/two.jpg";
+// }
+// function showImageThree() {
+//   var image3 = document.getElementById("three");
+//   image3.src = "photo/three.jpg";
+// }
+// function showImageFour() {
+//   var image4 = document.getElementById("four");
+//   image4.src = "photo/four.jpg";
+// }
+// function showImageFive() {
+//   var image5 = document.getElementById("five");
+//   image5.src = "photo/five.jpg";
+// }
