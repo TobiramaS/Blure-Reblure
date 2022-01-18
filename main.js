8291a9b619badc45b8860dfe16fae5ebec5cec62
TobiramaS <mkoryakovskyyoff@gmail.com>
2022-01-18 22:13:36 +0100

    fast reblure

--- main.js
+++ main.js
@@ -18,19 +18,22 @@ window.onload = init;
 function init() {
   var images = document.getElementsByTagName("img");
   for (var i = 0; i < images.length++; i++) {
-    images[i].onclick = showAnswer;
+    // images[i].onclick = showAnswer;
+    images[i].onmouseover = showAnswer;
+    images[i].onmouseout = reblur;
   }
 }
 
-function showAnswer(eventObj) {
-  var image = eventObj.target;
+function showAnswer(foto) {
+  var image = foto.target;
   var name = image.id;
   name = "photo/" + name + ".JPG";
   image.src = name;
-  setTimeout(reblur, 2000, image);
+  // setTimeout(reblur, 2000, image);
 }
 
-function reblur(image) {
+function reblur(foto) {
+  var image = foto.target;
   var name = image.id;
   name = "photo/" + name + "blur.jpg";
   image.src = name;
